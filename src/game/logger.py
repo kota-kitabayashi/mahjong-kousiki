38e408701f5be1d9eeda23d41873579592aa755e
kota-kitabayashi <koutagon17@icloud.com>
2026-03-14 20:43:36 +0900

    docs(logger): MahjongLoggerクラスの説明追記

--- src/game/logger.py
+++ src/game/logger.py
@@ -6,10 +6,13 @@ from pathlib import Path
 
 
 class MahjongLogger:
+    # 麻雀ログクラス
     def __init__(self, path: str = 'logs/mahjong_log.txt') -> None:
+        # このクラス生成時にpathを再設定し、テキストを空にする
         self.path = Path(path)
         self.path.write_text('', encoding='utf-8')
 
     def log(self, message: str) -> None:
+        # ログを追記する
         with self.path.open('a', encoding='utf-8') as f:
             f.write(message + '\n')
