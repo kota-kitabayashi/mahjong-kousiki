b467008cdd80d939b1557a5bf0819042322fcde5
kota-kitabayashi <koutagon17@icloud.com>
2026-03-23 22:42:45 +0900

    docs(logger): コメントアウトの書き方を是正

--- src/game/logger.py
+++ src/game/logger.py
@@ -5,14 +5,14 @@ from __future__ import annotations
 from pathlib import Path
 
 
+# 麻雀ログクラス
 class MahjongLogger:
-    # 麻雀ログクラス
+    # このクラス生成時にpathを再設定し、テキストを空にする
     def __init__(self, path: str = 'logs/mahjong_log.txt') -> None:
-        # このクラス生成時にpathを再設定し、テキストを空にする
         self.path = Path(path)
         self.path.write_text('', encoding='utf-8')
 
+    # ログを追記する
     def log(self, message: str) -> None:
-        # ログを追記する
         with self.path.open('a', encoding='utf-8') as f:
             f.write(message + '\n')
